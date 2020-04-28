6c122d8fd8fa2652a103f57e698afb81547ff66a
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-04-28 15:25:45 +0100

    Created convert Raindrops function

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const convert = (drop) => {
-    return (
-     //type function here
-    );
+    if (drop % 7 == 0 && drop % 3 == 0 && drop % 5 == 0){
+        drip = "PlingPlangPlong";
+    }else if (drop % 3 == 0 && drop % 5 == 0){
+      drip = "PlingPlang";
+    }else if(drop % 7 == 0 && drop % 5 == 0){
+      drip = "PlangPlong";
+    }else if (drop % 7 == 0 && drop % 3 == 0){
+      drip = "PlingPlong";
+    }else if (drop % 7 == 0){
+      drip = "Plong";
+    }else if (drop % 3 == 0){
+      drip = "Pling";
+    }else if (drop % 5 == 0){
+      drip = "Plang";
+    }else{
+        return drop.toString(10);
+    }
+    return drip;
   };
-
-  module.exports = convert;
+  module.exports = convert
